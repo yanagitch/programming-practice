1d1f07274c92c1a197f247e0d17f590d6f4fc023
yanagitch <yanagi.m0703@gmail.com>
2023-03-07 22:11:22 +0900

    2023/03/07

--- registration/regist.php
+++ registration/regist.php
@@ -11,50 +11,52 @@
         <main>
             <h3>アカウント登録画面</h3>
             <form method = "post" action = "regist_confirm.php">
-                <div>
-                    <div>
+                <div style = "width:50%;">
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>名前（姓）</label>
                         <input type = "text" pattern= "[\u4E00-\u9FFF\u3040-\u309F]*" class = "text" size = "35" maxlength = "10" name = "family_name"
-                        value = "<?php echo $_POST['family_name']?>">
+                        value = "<?php echo $_POST['family_name']?>" required>
                     </div>
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>名前（名）</label>
                         <input type = "text" pattern = "[\u4E00-\u9FFF\u3040-\u309F]*" class = "text" size = "35" maxlength = "10" name = "last_name"
-                        value = "<?php echo $_POST['last_name']; ?>">
+                        value = "<?php echo $_POST['last_name']; ?>" required>
                     </div>
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>カナ（姓）</label>
                         <input type = "text" pattern="[\u30A1-\u30F6]*" class = "text" size = "35" maxlength = "10" name = "family_name_kana"
-                        value = "<?php echo $_POST['family_name_kana']; ?>">
+                        value = "<?php echo $_POST['family_name_kana']; ?>" required>
                     </div>
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>カナ（名）</label>
                         <input type = "text" pattern="[\u30A1-\u30F6]*" class = "text" size = "35" maxlength = "10" name = "last_name_kana"
-                        value = "<?php echo $_POST['last_name_kana']; ?>">
+                        value = "<?php echo $_POST['last_name_kana']; ?>" required>
                     </div>
-                    <div>
-                        <label>メールアドレス</label>
-                        <input type = "text"  pattern="[\d@-]*" class = "text" size = "35" maxlength = "100" name = "mail"
-                        value = "<?php echo $_POST['mail']; ?>">
+                    <div style="display:flex; justify-cocntent:space-between;">
+                        <label style="margin-right:15px;">メールアドレス</label>
+                        <input type = "text"  pattern="[\d\@-]*" class = "email" size = "35" maxlength = "100" name = "mail"
+                        value = "<?php echo $_POST['mail']; ?>" required>
                     </div>
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>パスワード</label>
                         <input type = "password" pattern="[\w]*" class = "text" size = "35" maxlength = "10" name = "password"
-                        value = "<?php echo $_POST['password']; ?>">
+                        value = "<?php echo $_POST['password']; ?>" required>
                     </div>
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label class="gender">性別</label>
-                        <input type = "radio" class = "radio" name = "gender" value = "0" checked = "checked">男
-                        <input type = "radio" class = "radio" name = "gender" value = "1">女
+                       <span>
+                         <input type = "radio" class = "radio" name = "gender" value = "0" checked = "checked">男
+                         <input type = "radio" class = "radio" name = "gender" value = "1">女
+                        </span>
                     </div> 
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>郵便番号</label>
                         <input type = "text" pattern = "[\d]*" class = "text" size = "25" maxlength = "7" name = "postal_code"
-                        value = "<?php echo $_POST['postal_code']; ?>">
+                        value = "<?php echo $_POST['postal_code']; ?>" style="margin-left:43px;" required>
                     </div>
-                    <div>
+                    <div style="display:flex; justify-cocntent:space-between;">
                         <label>住所（都道府県）</label>
-                        <select name = "prefecture">
+                        <select name = "prefecture" style="margin-left:-20px;" required>
                             <option></option> <option>北海道</option>
                             <option>青森県</option> <option>岩手県</option>
                             <option>宮城県</option> <option>秋田県</option>
@@ -81,23 +83,23 @@
                             <option>鹿児島県</option> <option>沖縄県</option>
                         </select>
                     </div>
-                        <div>
+                        <div style="display:flex; justify-cocntent:space-between;">
                             <label>住所（市区町村)</label>
-                            <input type = "text" pattern = "[^\x01-\x7E-\u4E00-\u9FFF\u3040-\u309F-\u30A1-\u30F6-\d ]*" class = "text" size = "35" maxlength = "10" name = "address_1"
-                            value = "<?php echo $_POST['address_1']; ?>">
+                            <input type = "text" pattern = "[０-９\u4E00-\u9FFF\u3040-\u309F-\u30A1-\u30F6-\d ]*" class = "text" size = "35" maxlength = "10" name = "address_1"
+                            value = "<?php echo $_POST['address_1']; ?>" style="margin-left:-12px;" required>
                         </div>
-                        <div>
+                        <div style="display:flex; justify-cocntent:space-between;">
                             <label>住所（番地）</label>
-                            <input type = "text" pattern = "[^\x01-\x7E-\u4E00-\u9FFF\u3040-\u309F-\u30A1-\u30F6-\d ]*" class = "text" size = "35" maxlength = "100" name = "address_2"
-                            value = "<?php echo $_POST['address_2']; ?>">
+                            <input type = "text" pattern = "[０-９\u4E00-\u9FFF\u3040-\u309F-\u30A1-\u30F6-\d ]*" class = "text" size = "35" maxlength = "100" name = "address_2"
+                            value = "<?php echo $_POST['address_2']; ?>" style="margin-left:9px;" required>
                         </div>
-                        <duv>
+                        <duv style="display:flex; justify-cocntent:space-between;">
                             <label>アカウント権限</label>
-                            <select name = "authority">
+                            <select name = "authority" style="margin-left:-7px;">
                                 <option value = "0">一般</option>
                                 <option value = "1">管理者</option>
                             </select>
-                </div>
+                        </div>
                     <div class = "button">
                         <button type = "submit" name = "confirm">確認する</button>
                     </div>
--- registration/regist_complete.php
+++ registration/regist_complete.php
@@ -1,13 +1,11 @@
 <?php
-
     $_POST['gender']=(int) $_POST['gender']; 
     $_POST['postal_code']=(int) $_POST['postal_code'];
     $_POST['authority']=(int) $_POST['authority'];
     $_POST['password']=password_hash($_POST['password'],PASSWORD_DEFAULT);
 
-
     mb_internal_encoding("utf8");
-    
+
     try{
         $pdo = new PDO("mysql:dbname=registration;host=localhost;","root","root");
     }catch(PDOException $error){
@@ -16,9 +14,7 @@
 
    $pdo->exec("insert into registration (family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority)
     values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$_POST['password']."','".$_POST['gender']."',
-    '".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."');");
-
-    
+    '".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."');");  
 ?>
 
 <!DOCTYPE html>
